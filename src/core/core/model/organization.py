415c915afee98171037cb0a16bb0357b9cbb5ddf
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-01-25 10:35:32 +0000

    Pytest: Creating an organization fails
    Creating an organization with an id throws the following error:
    
    "TypeError: __init__() got an unexpected keyword argument 'id'"
    
    This occurs because the organization model doesn't have an
    parameter for id.

--- src/core/core/model/organization.py
+++ src/core/core/model/organization.py
@@ -22,8 +22,8 @@ class Organization(db.Model):
     address_id = db.Column(db.Integer, db.ForeignKey("address.id"))
     address = db.relationship("Address", cascade="all")
 
-    def __init__(self, name, description, address):
-        self.id = None
+    def __init__(self, name, description, address, id=None):
+        self.id = id
         self.name = name
         self.description = description
         self.address = address
