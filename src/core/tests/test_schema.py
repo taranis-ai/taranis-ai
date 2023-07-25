ee3ba4118046c33bb39992136168dc6056f866f0
ewoiset <elisabeth.woisetschlaeger@ait.ac.at>
2023-07-25 12:01:10 +0200

    Add schema validation

--- src/core/tests/test_schema.py
+++ src/core/tests/test_schema.py
@@ -1,5 +1,6 @@
 import schemathesis
 import pytest
+from hypothesis import settings, Verbosity
 
 
 @pytest.fixture
@@ -7,10 +8,11 @@ def schema_wsgi(app):
     return schemathesis.from_wsgi("/api/v1/doc/swagger.json", app)
 
 
-schema = schemathesis.from_pytest_fixture("schema_wsgi")
+schema = schemathesis.from_pytest_fixture("schema_wsgi", validate_schema=True)
 
 
 @schema.parametrize()
+@settings(verbosity=Verbosity.debug)
 def test_schema(case, access_token, client):
     response = case.call_wsgi(headers={"Authorization": access_token})
     case.validate_response(response)
