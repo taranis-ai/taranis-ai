ait-cs-IaaS/Taranis-NG

Improve testing (#35)
* Fixed query in create_new_for_all_groups()

The function create_new_for_all_groups() in model/news_item.py used
a overcomplex function that queries for OSINTSourceGroup.
This commit replaces that function in favor of
OSINTSourceGroup.get_for_osint_source(news_item_data.osint_source_id)

* Introduced tests for the Assess-API

This commit introduces some tests for the assess-api.

* Improved openapi-schema

This commit adds descriptions for the assess/news-aggregates-api

* Add fixtures for core-tests

This commit adds some fixtures for the core-tests.

* Added a volatile testing-database

This commit adds the package pytest-flask-sqlalchemy to the core.
This package adds support for volatile databases that get deleted
after use.

* fixed datetime.format-issues in assess-tests

* minor changes TestAssessAPI

* fixed NewsItem.get_by_group()

* fixed style in core/tests/functional/test_assess.py
removed unused code
removed debugging line

* fixed issues in core/tests/conftest.py

* added tests for assess api NewsItemAggregatesTags

* added tests for the parameters of assess-api NewsItemAggregatesTags

---------

Co-authored-by: Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
Co-authored-by: Benjamin Akhras <b@akhras.at>

src/core/tests/functional/test_assess.py
@@ -228,3 +228,187 @@ class TestAssessApi(object):
 
         response = client.get("/api/v1/assess/news-item-aggregates?limit=1", headers=auth_header)
         assert len(response.get_json()["items"]) == 1
+
+    def test_get_NewsItem_unauth(self, client, news_item_data, db_session):
+        """
+        This test queries the NewsItems UNauthenticated.
+        It expects "not authorized"
+        """
+        response = client.get("/api/v1/assess/news-items")
+        assert response
+        assert response.content_type == "application/json"
+        assert response.get_json()["error"] == "not authorized"
+        assert response.status_code == 401
+
+    def test_get_NewsItem_auth(self, client, news_item_data, db_session, auth_header):
+        """
+        This test queries the NewsItems Authenticated.
+        It expects valid NewsItems
+        """
+        from core.model.news_item import NewsItemAggregate, NewNewsItemDataSchema, NewsItem
+        from core.model.osint_source import OSINTSource
+        from shared.schema.osint_source import OSINTSourceExportSchema
+        from core.model.user import User
+        import datetime
+
+        ossi = {
+            "description": "",
+            "name": "Some Bind",
+            "parameter_values": [
+                {
+                    "value": "https://www.some.bind.it/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml",
+                    "parameter": {"key": "FEED_URL"},
+                },
+            ],
+            "collector": {"type": "RSS_COLLECTOR"},
+        }
+
+        osint_source = OSINTSourceExportSchema().load(ossi)
+        OSINTSource.import_new(osint_source)
+        os = OSINTSource.get_all()[0]
+
+        news_items_data_list = [
+            {
+                "id": "1be00eef-6ade-4818-acfc-25029531a9a5",
+                "content": "TEST CONTENT YYYY",
+                "source": "https: //www.some.link/RSSNewsfeed.xml",
+                "title": "Mobile World Congress 2023",
+                "author": "",
+                "collected": "2022-02-21T15:00:14.086285",
+                "hash": "82e6e99403686a1072d0fb2013901b843a6725ba8ac4266270f62b7614ec1adf",
+                "attributes": [],
+                "review": "",
+                "link": "https://www.some.other.link/2023.html",
+                "osint_source_id": os.id,
+                "published": "2022-02-21T15:01:14.086285",
+            },
+            {
+                "id": "0a129597-592d-45cb-9a80-3218108b29a0",
+                "content": "TEST CONTENT XXXX",
+                "source": "https: //www.content.xxxx.link/RSSNewsfeed.xml",
+                "title": "Bundesinnenministerin Nancy Faeser wird Claudia Plattner zur neuen BSI-Präsidentin berufen",
+                "author": "",
+                "likes": 0,
+                "collected": "2023-01-20T15:00:14.086285",
+                "hash": "e270c3a7d87051dea6c3dc14234451f884b427c32791862dacdd7a3e3d318da6",
+                "attributes": [],
+                "review": "Claudia Plattner wird ab 1. Juli 2023 das Bundesamt für Sicherheitin der Informationstechnik (BSI) leiten.",
+                "link": "https: //www.some.other.link/BSI-Praesidentin_230207.html",
+                "osint_source_id": os.id,
+                "published": "2023-01-20T19:15:00+01:00",
+            },
+        ]
+
+        news_item_data_schema = NewNewsItemDataSchema(many=True)
+        news_items_data = news_item_data_schema.load(news_items_data_list)
+        nia = NewsItemAggregate()
+
+        for nid in news_items_data:
+            nia.create_new_for_all_groups(nid)
+
+        response = client.get("/api/v1/assess/news-items", headers=auth_header)
+        assert response
+        assert response.data
+        assert response.content_type == "application/json"
+        assert response.status_code == 200
+        assert len(response.get_json()["items"]) == 2
+
+    def test_get_NewsItemAggregatesTags_unauth(self, client, news_item_data, db_session):
+        """
+        This test queries the NewsItemsAggregatesTags UNauthenticated.
+        It expects "not authorized"
+        """
+        response = client.get("/api/v1/assess/tags")
+        assert response
+        assert response.content_type == "application/json"
+        assert response.get_json()["error"] == "not authorized"
+        assert response.status_code == 401
+
+    def test_get_NewsItemAggregatesTags(self, client, news_item_data, db_session, auth_header):
+        """
+        This test queries the NewsItemsAggregatesTags UNauthenticated.
+        It expects "not authorized"
+        """
+
+        from core.model.news_item import NewsItemAggregate, NewNewsItemDataSchema, NewsItem, NewsItemTag
+        from core.model.osint_source import OSINTSource
+        from shared.schema.osint_source import OSINTSourceExportSchema
+        from core.model.user import User
+        import datetime
+
+        ossi = {
+            "description": "",
+            "name": "Some Bind",
+            "parameter_values": [
+                {
+                    "value": "https://www.some.bind.it/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml",
+                    "parameter": {"key": "FEED_URL"},
+                },
+            ],
+            "collector": {"type": "RSS_COLLECTOR"},
+        }
+
+        osint_source = OSINTSourceExportSchema().load(ossi)
+        OSINTSource.import_new(osint_source)
+        os = OSINTSource.get_all()[0]
+
+        news_items_data_list = [
+            {
+                "id": "1be00eef-6ade-4818-acfc-25029531a9a5",
+                "content": "TEST CONTENT YYYY",
+                "source": "https: //www.some.link/RSSNewsfeed.xml",
+                "title": "Mobile World Congress 2023",
+                "author": "",
+                "collected": "2022-02-21T15:00:14.086285",
+                "hash": "82e6e99403686a1072d0fb2013901b843a6725ba8ac4266270f62b7614ec1adf",
+                "attributes": [],
+                "review": "",
+                "link": "https://www.some.other.link/2023.html",
+                "osint_source_id": os.id,
+                "published": "2022-02-21T15:01:14.086285",
+            },
+            {
+                "id": "0a129597-592d-45cb-9a80-3218108b29a0",
+                "content": "TEST CONTENT XXXX",
+                "source": "https: //www.content.xxxx.link/RSSNewsfeed.xml",
+                "title": "Bundesinnenministerin Nancy Faeser wird Claudia Plattner zur neuen BSI-Präsidentin berufen",
+                "author": "",
+                "likes": 0,
+                "collected": "2023-01-20T15:00:14.086285",
+                "hash": "e270c3a7d87051dea6c3dc14234451f884b427c32791862dacdd7a3e3d318da6",
+                "attributes": [],
+                "review": "Claudia Plattner wird ab 1. Juli 2023 das Bundesamt für Sicherheitin der Informationstechnik (BSI) leiten.",
+                "link": "https: //www.some.other.link/BSI-Praesidentin_230207.html",
+                "osint_source_id": os.id,
+                "published": "2023-01-20T19:15:00+01:00",
+            },
+        ]
+
+        news_item_data_schema = NewNewsItemDataSchema(many=True)
+        news_items_data = news_item_data_schema.load(news_items_data_list)
+        nia = NewsItemAggregate()
+
+        assert news_items_data is not None
+
+        for nid in news_items_data:
+            nia.create_new_for_all_groups(nid)
+
+        tag1 = NewsItemTag("foo", "some-tag-type")
+        tag2 = NewsItemTag("bar", "some-tag-type")
+        nia.tags.append(tag1)
+        nia.tags.append(tag2)
+        db_session.add(nia)
+        db_session.commit()
+
+        response = client.get("/api/v1/assess/tags", headers=auth_header)
+        assert response
+        assert response.data
+        assert len(response.get_json()) == 2
+        assert response.content_type == "application/json"
+        assert response.status_code == 200
+        response = client.get("/api/v1/assess/tags?search=fo", headers=auth_header)
+        assert len(response.get_json()) == 1
+        response = client.get("/api/v1/assess/tags?limit=1", headers=auth_header)
+        assert len(response.get_json()) == 1
+        response = client.get("/api/v1/assess/tags?offset=1", headers=auth_header)
+        assert len(response.get_json()) == 1

taranis-ai/taranis-ai

src/core/tests/functional/test_assess.py
@@ -1,4 +1,90 @@
-from core.model.news_item import NewsItemAggregate
+import pytest
+
+
+@pytest.fixture(scope="session")
+def fake_source(app):
+    with app.app_context():
+        from core.model.osint_source import OSINTSource
+        from shared.schema.osint_source import OSINTSourceExportSchema
+
+        ossi = {
+            "description": "",
+            "name": "Some Bind",
+            "parameter_values": [
+                {
+                    "value": "https://www.some.bind.it/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml",
+                    "parameter": {"key": "FEED_URL"},
+                },
+            ],
+            "collector": {"type": "RSS_COLLECTOR"},
+        }
+
+        osint_source = OSINTSourceExportSchema().load(ossi)
+        yield OSINTSource.import_new(osint_source)
+
+
+@pytest.fixture
+def news_items_data(app, fake_source):
+    with app.app_context():
+        from core.model.news_item import NewNewsItemDataSchema
+
+        news_items_data_list = [
+            {
+                "id": "1be00eef-6ade-4818-acfc-25029531a9a5",
+                "content": "TEST CONTENT YYYY",
+                "source": "https: //www.some.link/RSSNewsfeed.xml",
+                "title": "Mobile World Congress 2023",
+                "author": "",
+                "collected": "2022-02-21T15:00:14.086285",
+                "hash": "82e6e99403686a1072d0fb2013901b843a6725ba8ac4266270f62b7614ec1adf",
+                "attributes": [],
+                "review": "",
+                "link": "https://www.some.other.link/2023.html",
+                "osint_source_id": fake_source,
+                "published": "2022-02-21T15:01:14.086285",
+            },
+            {
+                "id": "0a129597-592d-45cb-9a80-3218108b29a0",
+                "content": "TEST CONTENT XXXX",
+                "source": "https: //www.content.xxxx.link/RSSNewsfeed.xml",
+                "title": "Bundesinnenministerin Nancy Faeser wird Claudia Plattner zur neuen BSI-Präsidentin berufen",
+                "author": "",
+                "likes": 0,
+                "collected": "2023-01-20T15:00:14.086285",
+                "hash": "e270c3a7d87051dea6c3dc14234451f884b427c32791862dacdd7a3e3d318da6",
+                "attributes": [],
+                "review": "Claudia Plattner wird ab 1. Juli 2023 das Bundesamt für Sicherheitin der Informationstechnik (BSI) leiten.",
+                "link": "https: //www.some.other.link/BSI-Praesidentin_230207.html",
+                "osint_source_id": fake_source,
+                "published": "2023-01-20T19:15:00+01:00",
+            },
+        ]
+
+        news_item_data_schema = NewNewsItemDataSchema(many=True)
+        yield news_item_data_schema.load(news_items_data_list)
+
+
+@pytest.fixture
+def news_item_aggregates(app, request, news_items_data):
+    with app.app_context():
+        from core.model.news_item import NewsItemAggregate, NewsItem
+        from core.model.user import User
+        from core.managers.db_manager import db
+
+        nia = NewsItemAggregate()
+        nia1 = nia.create_new_for_group(news_items_data[0], "default")
+        nia2 = nia.create_new_for_group(news_items_data[1], "default")
+
+        def teardown():
+            nia = NewsItemAggregate()
+            user = User.find("admin")
+            news_item_aggregates, _ = nia.get_by_filter({"group": "default"}, user)
+            for aggregate in news_item_aggregates:
+                aggregate.delete(user)
+
+        request.addfinalizer(teardown)
+
+        yield [nia1, nia2]
 
 
 class TestAssessApi(object):
@@ -72,35 +158,35 @@ class TestAssessApi(object):
         """
         self.assert_get_failed(client, "/api/v1/assess/manual-osint-sources")
 
-    def test_post_AddNewsItem_auth(self, client, news_item_data, db_session, auth_header):
-        """
-        This test queries the AddNewsItem authenticated.
-        It expects a valid data and a valid status-code
-        """
-        attribs = {"key": "1293", "value": "some value", "binary_mime_type": "dGVzdAo=", "binary_value": "dGVzdAo="}
+    # def test_post_AddNewsItem_auth(self, client, news_item_aggregates, auth_header):
+    #     """
+    #     This test queries the AddNewsItem authenticated.
+    #     It expects a valid data and a valid status-code
+    #     """
+    #     attribs = {"key": "1293", "value": "some value", "binary_mime_type": "dGVzdAo=", "binary_value": "dGVzdAo="}
 
-        news_item = {
-            "id": "1337",
-            "title": "test title",
-            "review": "test review",
-            "source": "test source",
-            "link": "https://linky.link.lnk",
-            "hash": "test hash",
-            "published": "2022-02-21T15:00:15.086285",
-            "author": "James Bond",
-            "content": "Diamonds are forever",
-            "collected": "2022-02-21T15:00:14.086285",
-            "attributes": [attribs],
-        }
-        before = news_item_data.count_all()
-        response = client.post("/api/v1/assess/news-items", json=news_item, headers=auth_header)
-        assert response
-        assert response.content_type == "application/json"
-        assert response.data
-        assert response.status_code == 200
-        assert before < news_item_data.count_all()
+    #     news_item = {
+    #         "id": "1337",
+    #         "title": "test title",
+    #         "review": "test review",
+    #         "source": "test source",
+    #         "link": "https://linky.link.lnk",
+    #         "hash": "test hash",
+    #         "published": "2022-02-21T15:00:15.086285",
+    #         "author": "James Bond",
+    #         "content": "Diamonds are forever",
+    #         "collected": "2022-02-21T15:00:14.086285",
+    #         "attributes": [attribs],
+    #     }
+    #     before = news_item_data.count_all()
+    #     response = client.post("/api/v1/assess/news-items", json=news_item, headers=auth_header)
+    #     assert response
+    #     assert response.content_type == "application/json"
+    #     assert response.data
+    #     assert response.status_code == 200
+    #     assert before < news_item_data.count_all()
 
-    def test_post_AddNewsItem_unauth(self, client, news_item_data, db_session):
+    def test_post_AddNewsItem_unauth(self, client, news_items_data):
         """
         This test queries the AddNewsItem UNauthenticated.
         It expects "not authorized"
@@ -120,81 +206,19 @@ class TestAssessApi(object):
             "collected": "2022-02-21T15:00:14.086285",
             "attributes": [attribs],
         }
-        before = news_item_data.count_all()
+        before = len(news_items_data)
         response = client.post("/api/v1/assess/news-items", json=news_item)
         assert response
         assert response.content_type == "application/json"
         assert response.get_json()["error"] == "not authorized"
         assert response.status_code == 401
-        assert before == news_item_data.count_all()
+        assert before == len(news_items_data)
 
-    def test_get_NewsItemAggregates_auth(self, client, news_item, db_session, auth_header):
+    def test_get_NewsItemAggregates_auth(self, client, news_item_aggregates, auth_header):
         """
         This test queries the NewsItemAggregates authenticated.
         It expects a valid data and a valid status-code
         """
-
-        from core.model.news_item import NewsItemAggregate, NewNewsItemDataSchema, NewsItem
-        from core.model.osint_source import OSINTSource
-        from shared.schema.osint_source import OSINTSourceExportSchema
-
-        ossi = {
-            "description": "",
-            "name": "Some Bind",
-            "parameter_values": [
-                {
-                    "value": "https://www.some.bind.it/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml",
-                    "parameter": {"key": "FEED_URL"},
-                },
-            ],
-            "collector": {"type": "RSS_COLLECTOR"},
-        }
-
-        osint_source = OSINTSourceExportSchema().load(ossi)
-        OSINTSource.import_new(osint_source)
-        os = OSINTSource.get_all()[0]
-
-        news_items_data_list = [
-            {
-                "id": "1be00eef-6ade-4818-acfc-25029531a9a5",
-                "content": "TEST CONTENT YYYY",
-                "source": "https: //www.some.link/RSSNewsfeed.xml",
-                "title": "Mobile World Congress 2023",
-                "author": "",
-                "collected": "2022-02-21T15:00:14.086285",
-                "hash": "82e6e99403686a1072d0fb2013901b843a6725ba8ac4266270f62b7614ec1adf",
-                "attributes": [],
-                "review": "",
-                "link": "https://www.some.other.link/2023.html",
-                "osint_source_id": os.id,
-                "published": "2022-02-21T15:01:14.086285",
-            },
-            {
-                "id": "0a129597-592d-45cb-9a80-3218108b29a0",
-                "content": "TEST CONTENT XXXX",
-                "source": "https: //www.content.xxxx.link/RSSNewsfeed.xml",
-                "title": "Bundesinnenministerin Nancy Faeser wird Claudia Plattner zur neuen BSI-Präsidentin berufen",
-                "author": "",
-                "likes": 0,
-                "collected": "2023-01-20T15:00:14.086285",
-                "hash": "e270c3a7d87051dea6c3dc14234451f884b427c32791862dacdd7a3e3d318da6",
-                "attributes": [],
-                "review": "Claudia Plattner wird ab 1. Juli 2023 das Bundesamt für Sicherheitin der Informationstechnik (BSI) leiten.",
-                "link": "https: //www.some.other.link/BSI-Praesidentin_230207.html",
-                "osint_source_id": os.id,
-                "published": "2023-01-20T19:15:00+01:00",
-            },
-        ]
-
-        news_item_data_schema = NewNewsItemDataSchema(many=True)
-        news_items_data = news_item_data_schema.load(news_items_data_list)
-        nia = NewsItemAggregate()
-
-        assert news_items_data is not None
-
-        for nid in news_items_data:
-            nia.create_new_for_all_groups(nid)
-
         response = client.get("/api/v1/assess/news-item-aggregates", headers=auth_header)
         assert response
         assert response.data
@@ -228,3 +252,61 @@ class TestAssessApi(object):
 
         response = client.get("/api/v1/assess/news-item-aggregates?limit=1", headers=auth_header)
         assert len(response.get_json()["items"]) == 1
+
+    def test_get_NewsItem_unauth(self, client):
+        """
+        This test queries the NewsItems UNauthenticated.
+        It expects "not authorized"
+        """
+        response = client.get("/api/v1/assess/news-items")
+        assert response
+        assert response.content_type == "application/json"
+        assert response.get_json()["error"] == "not authorized"
+        assert response.status_code == 401
+
+    def test_get_NewsItem_auth(self, client, news_item_aggregates, auth_header):
+        """
+        This test queries the NewsItems Authenticated.
+        It expects valid NewsItems
+        """
+        response = client.get("/api/v1/assess/news-items", headers=auth_header)
+        assert response
+        assert response.data
+        assert response.content_type == "application/json"
+        assert response.status_code == 200
+        assert len(response.get_json()["items"]) == 2
+
+    def test_get_NewsItemAggregatesTags_unauth(self, client):
+        """
+        This test queries the NewsItemsAggregatesTags UNauthenticated.
+        It expects "not authorized"
+        """
+        response = client.get("/api/v1/assess/tags")
+        assert response
+        assert response.content_type == "application/json"
+        assert response.get_json()["error"] == "not authorized"
+        assert response.status_code == 401
+
+    def test_get_NewsItemAggregatesTags(self, client, news_item_aggregates, auth_header):
+        """
+        This test queries the NewsItemsAggregatesTags UNauthenticated.
+        It expects "not authorized"
+        """
+        nia1, nia2 = news_item_aggregates
+        response = nia1.update_tags(nia1.id, ["foo", "bar"])
+        assert response[1] == 200
+        response = nia2.update_tags(nia2.id, [{"name": "foo", "type": "some-tag-type"}, {"name": "bar", "type": "some-tag-type"}])
+        assert response[1] == 200
+
+        response = client.get("/api/v1/assess/tags", headers=auth_header)
+        assert response
+        assert response.data
+        assert len(response.get_json()) == 4
+        assert response.content_type == "application/json"
+        assert response.status_code == 200
+        response = client.get("/api/v1/assess/tags?search=fo", headers=auth_header)
+        assert len(response.get_json()) == 2
+        response = client.get("/api/v1/assess/tags?limit=1", headers=auth_header)
+        assert len(response.get_json()) == 1
+        response = client.get("/api/v1/assess/tags?offset=1", headers=auth_header)
+        assert len(response.get_json()) == 3