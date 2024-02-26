2c4d5f8c83e9f2b015aed9d386d1cca24d462d10
Elisabeth Woisetschlaeger <elisabeth.woisetschlaeger@ait.ac.at>
2024-02-26 19:51:32 +0100

    Update clustering task

--- src/worker/worker/story_clustering/clustering_tasks.py
+++ src/worker/worker/story_clustering/clustering_tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
 import worker.bots
-from worker.log import logger
 import jsonpickle
+from celery import current_app
 
 
 @shared_task(name="initial_clustering")
 def initial_clustering(corpus_encoded):
     corpus = jsonpickle.decode(corpus_encoded)
-    story_bot = worker.bots.StoryBot()
+    story_bot = current_app.tasks.get("bot_task").bots.get("story_bot")
     story_bot.initial_clustering_event_handler(corpus)
