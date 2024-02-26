22bc8571ae878a50269a10dd79b5cc19919dcd8c
Elisabeth Woisetschlaeger <elisabeth.woisetschlaeger@ait.ac.at>
2024-02-26 17:17:49 +0100

    Add new clustering task

---
+++ src/worker/worker/story_clustering/clustering_tasks.py
@@ -0,0 +1,10 @@
+from celery import shared_task
+import worker.bots
+from worker.log import logger
+import jsonpickle
+
+@shared_task(name="initial_clustering")
+def initial_clustering(corpus_encoded):
+    corpus = jsonpickle.decode(corpus_encoded)
+    story_bot = worker.bots.StoryBot()
+    story_bot.initial_clustering_event_handler(corpus)
