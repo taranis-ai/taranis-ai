40f5e2926c045dec58e40adec42de2023da9e6b0
ewoiset <88492381+ewoiset@users.noreply.github.com>
2024-03-19 13:09:17 +0100

    Feature/story bot extensions (#156)
    - **New Features**
    	- Enhanced story bot functionality for improved clustering of stories, including asynchronous initial clustering and event handling for clustering events.

---
+++ src/worker/worker/story_clustering/clustering_tasks.py
@@ -0,0 +1,12 @@
+from celery import shared_task
+from celery import current_app
+import json
+from story_clustering.document_representation import Corpus
+
+
+@shared_task(name="initial_clustering")
+def initial_clustering(corpus_encoded):
+    corpus_dict = json.loads(corpus_encoded)
+    corpus = Corpus(**corpus_dict)
+    story_bot = current_app.tasks.get("bot_task").bots.get("story_bot")
+    story_bot.initial_clustering_event_handler(corpus)
