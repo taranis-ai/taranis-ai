13d8255d67b642b0a7edeccce796dcaa14e8042f
Peter Leitmann <peto.leitmann@gmail.com>
2024-03-25 14:31:20 +0100

    Refactor and clean up

--- src/worker/worker/publishers/email_publisher.py
+++ src/worker/worker/publishers/email_publisher.py
@@ -27,11 +27,11 @@ class EMAILPublisher(BasePublisher):
         self.name = "EMAIL Publisher"
         self.description = "Publisher for publishing by email"
 
-    def publish(self, publisher, publisher_input) -> str | None:
+    def publish(self, publisher, publisher_input) -> dict:
         self.smtp_address = publisher.get("parameters").get("SMTP_SERVER_ADDRESS")
         if not self.smtp_address:
             logger.error("No SMTP server address provided")
-            return "No SMTP server address provided"
+            return {"error": "No SMTP server address provided"}
 
         self.smtp_port = publisher.get("parameters").get("SMTP_SERVER_PORT", 25)
         self.smtp_tls = publisher.get("parameters").get("SERVER_TLS")
@@ -43,12 +43,17 @@ class EMAILPublisher(BasePublisher):
 
         if publisher_input is None:
             logger.error("No user input provided")
-            return "No user input provided"
+            return {"error": "No user input provided"}
 
         if rendered_product := self.get_rendered_product(publisher_input):
-            return self.setup_server_connection(rendered_product)
+            self.setup_email(rendered_product)
+
+            context = ssl.create_default_context() if self.smtp_tls else None
+
+            return self.send_with_tls(context) if context else self.send_without_tls()
+
         logger.warning("Could not get rendered Product")
-        return "Could not get rendered Product"
+        return {"error": "Could not get rendered Product"}
 
     def get_rendered_product(self, publisher_input) -> Product | None:
         product_id = publisher_input.get("id")
@@ -71,60 +76,54 @@ class EMAILPublisher(BasePublisher):
 
         file_name = f"product_{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M')}"
         maintype, subtype = rendered_product.mime_type.split("/")
-        logger.info("EMAIL Publisher: Creating attachment")
+        logger.debug("EMAIL Publisher: Creating attachment")
         attachment_data = base64.b64decode(rendered_product.data)
         self.msg.add_attachment(attachment_data, maintype=maintype, subtype=subtype, filename=f"{file_name}")
 
-    def setup_server_connection(self, rendered_product: Product) -> str:
+    def setup_email(self, rendered_product: Product):
         if rendered_product.mime_type in ["text/plain", "text/html"]:
             self.setup_simple_email(rendered_product)
         else:
             self.attach_file(rendered_product)
             logger.debug("Product attached")
 
-        logger.debug("EMAIL Publisher: Initiate connection to the SMTP sever.")
-
-        if self.smtp_tls:
-            context = ssl.create_default_context()
-            try:
-                with SMTP_SSL(self.smtp_address, self.smtp_port, context=context) as server:
-                    if self.smtp_username and self.smtp_password:
-                        self.smtp_login(server)
-                    return self.send_mail(server)
-            except Exception as e:
-                logger.error(f"Your SMTP server throws an error: {str(e)}")
-                return "Your SMTP server throws an error"
-        try:
-            server = smtplib.SMTP(self.smtp_address, self.smtp_port)
-            if self.smtp_username and self.smtp_password:
-                self.smtp_login(server)
-            self.send_mail(server)
-        except Exception as e:
-            logger.error(f"Your SMTP server throws an error: {str(e)}")
-            return "Your SMTP server throws an error}"
-
-        logger.info("EMAIL Publisher: Email sent")
-        return "Email sent successfully"
-
-    def send_mail(self, s) -> str:
-        # s.set_debuglevel(1)
-        if response := s.sendmail(self.msg.get("From"), self.msg.get("To"), self.msg.as_string()):
-            logger.warning(f"EMAIL Publisher: Response from SMTP server: {response}")
-            return "Your SMTP server throws an error"
-        s.quit()
-        return "Email sent successfully"
-
-    def smtp_login(self, server):
+    def smtp_login(self, server) -> dict | None:
         try:
             server.login(self.smtp_username, self.smtp_password)
-        except SMTPAuthenticationError:
-            return "Failed to log in to the SMTP server. Check your authentication credentials."
-        except SMTPException as e:
-            logger.error(f"Your SMTP server throws an error: {str(e)}")
-            return "An SMTP error occurred"
+            return {"message": "Email Publisher: Task Successful"}
+        except (SMTPAuthenticationError, SMTPException, Exception) as e:
+            error_message = "SMTP authentication error" if isinstance(e, SMTPAuthenticationError) else "An SMTP error occurred"
+            logger.error(f"{error_message}: {str(e)}")
+            return {"error": error_message}
+
+    def send_with_tls(self, context) -> dict | None:
+        try:
+            with SMTP_SSL(self.smtp_address, self.smtp_port, context=context) as server:
+                return self.send_mail(server)
+        except Exception as e:
+            logger.error(f"Your SMTP server with TLS context throws an error: {str(e)}")
+            return {"error": "An SMTP error occurred"}
+
+    def send_without_tls(self) -> dict | None:
+        try:
+            server = smtplib.SMTP(self.smtp_address, self.smtp_port)
+            return self.send_mail(server)
         except Exception as e:
             logger.error(f"Your SMTP server throws an error: {str(e)}")
-            return "An SMTP error occurred"
+            return {"error": "An SMTP error occurred"}
+
+    def send_mail(self, server) -> dict | None:
+        # s.set_debuglevel(1)
+        if self.smtp_username and self.smtp_password:
+            self.smtp_login(server)
+        try:
+            server.sendmail(self.msg.get("From"), self.msg.get("To"), self.msg.as_string())
+        except smtplib.SMTPException as e:
+            logger.error(f"Your SMTP server throws an error: {str(e)}")
+            return {"error": "An SMTP error occurred"}
+        finally:
+            server.quit()
+        return {"message": "Email Publisher: Task Successful"}
 
 
 if __name__ == "__main__":
