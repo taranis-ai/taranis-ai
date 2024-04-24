58abf3613e8dbfeb68c709e309b959b2219aae0a
Peter Leitmann <peto.leitmann@gmail.com>
2024-04-24 20:17:56 +0200

    WIP

--- src/core/tests/conftest.py
+++ src/core/tests/conftest.py
@@ -9,8 +9,12 @@ load_dotenv(dotenv_path="tests/.env", override=True)
 
 
 def pytest_addoption(parser):
-    parser.addoption("--run-e2e", action="store_true", default=False, help="run e2e tests")
-    parser.addoption("--run-e2e-ci", action="store_true", default=False, help="run e2e tests for CI")
+    parser.addoption(
+        "--run-e2e", action="store_true", default=False, help="run e2e tests"
+    )
+    parser.addoption(
+        "--run-e2e-ci", action="store_true", default=False, help="run e2e tests for CI"
+    )
 
 
 def pytest_collection_modifyitems(config, items):
@@ -30,9 +34,11 @@ def pytest_collection_modifyitems(config, items):
                 if "e2e_ci" not in item.keywords:
                     item.add_marker(skip_non_e2e_ci)
     else:
-        skip_e2e = pytest.mark.skip(reason="need --run-e2e or --run-e2e-ci option to run e2e tests")
+        skip_e2e = pytest.mark.skip(
+            reason="need --run-e2e or --run-e2e-ci option to run e2e tests"
+        )
         for item in items:
-            if "e2e" in item.keywords or "e2e-ci" in item.keywords:
+            if "e2e" in item.keywords or "e2e_ci" in item.keywords:
                 item.add_marker(skip_e2e)
 
 
@@ -109,7 +115,10 @@ def access_token(app):
 
 @pytest.fixture
 def auth_header(access_token):
-    return {"Authorization": f"Bearer {access_token}", "Content-type": "application/json"}
+    return {
+        "Authorization": f"Bearer {access_token}",
+        "Content-type": "application/json",
+    }
 
 
 @pytest.fixture
@@ -153,9 +162,15 @@ def access_token_no_permissions(app):
 
 @pytest.fixture
 def auth_header_no_permissions(access_token_no_permissions):
-    return {"Authorization": f"Bearer {access_token_no_permissions}", "Content-type": "application/json"}
+    return {
+        "Authorization": f"Bearer {access_token_no_permissions}",
+        "Content-type": "application/json",
+    }
 
 
 @pytest.fixture
 def auth_header_user_permissions(access_token_user_permissions):
-    return {"Authorization": f"Bearer {access_token_user_permissions}", "Content-type": "application/json"}
+    return {
+        "Authorization": f"Bearer {access_token_user_permissions}",
+        "Content-type": "application/json",
+    }
