678feda168ea014eccfad04e51e06ded954cd35b
Benjamin Akhras <b@akhras.at>
2024-04-29 16:17:45 +0200

    add warning if pytest called from wrong directory

--- src/worker/worker/tests/conftest.py
+++ src/worker/worker/tests/conftest.py
@@ -1,4 +1,11 @@
+import os
+import sys
 import pytest
+
+current_path = os.getcwd()
+
+if not current_path.endswith("src/worker"):
+    sys.exit("Tests must be run from within src/worker")
 
 
 @pytest.fixture(scope="session")
