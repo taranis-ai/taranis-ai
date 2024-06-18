ca9eac6b20e9eafe14675f749abfbb2d05414650
Peter Leitmann <peto.leitmann@gmail.com>
2024-06-18 12:03:26 +0200

    Fix logic of backup for podman

--- docker/backup.sh
+++ docker/backup.sh
@@ -6,5 +6,11 @@ backup_dir="backups/$(date +%FT%H%M%S)"
 
 mkdir -p "${backup_dir}"
 
-docker compose exec core     tar czv /app/data      > "${backup_dir}/core_data.tar.gz"
+TMP_CORE_NAME=$(docker compose ps -f  '{{.Names}}' | grep core || echo "Error: No running services found.")
+export TMP_CORE_NAME
+echo "Name of Core's container is: $TMP_CORE_NAME"
+
+docker exec $TMP_CORE_NAME   tar czv /app/data      > "${backup_dir}/core_data.tar.gz"
 docker compose exec database pg_dumpall -U taranis  > "${backup_dir}/database_backup.sql"
+
+unset TMP_CORE_NAME
