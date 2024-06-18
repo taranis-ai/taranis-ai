17d76df9b86d0385441c1e524a1aaf34c8b4e427
Peter Leitmann <peto.leitmann@gmail.com>
2024-06-18 15:29:00 +0200

    refactor backup script

--- docker/backup.sh
+++ docker/backup.sh
@@ -6,11 +6,6 @@ backup_dir="backups/$(date +%FT%H%M%S)"
 
 mkdir -p "${backup_dir}"
 
-TMP_CORE_NAME=$(docker compose ps -f  '{{.Names}}' | grep core || echo "Error: No running services found.")
-export TMP_CORE_NAME
-echo "Name of Core's container is: $TMP_CORE_NAME"
-
-docker exec $TMP_CORE_NAME   tar czv /app/data      > "${backup_dir}/core_data.tar.gz"
-docker compose exec database pg_dumpall -U taranis  > "${backup_dir}/database_backup.sql"
-
-unset TMP_CORE_NAME
+docker compose exec core     tar czvf core_data.tar.gz /app/data    > "${backup_dir}/core_data.tar.gz"
+docker compose exec core     rm core_data.tar.gz
+docker compose exec database pg_dumpall -U taranis                  > "${backup_dir}/database_backup.sql"
