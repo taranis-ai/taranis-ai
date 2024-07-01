9d1f084f61f7cb61859740577c47e2574f28cbce
Peter Leitmann <peto.leitmann@gmail.com>
2024-07-01 15:41:54 +0200

    Fix restore and backup script

--- docker/backup.sh
+++ docker/backup.sh
@@ -8,8 +8,7 @@ mkdir -p "${backup_dir}"
 
 export TMP_CORE_NAME=$(docker compose ps -f '{{.Names}}' | grep core || exit "Error: No running services found.")
 
-docker compose exec core     tar czvf /tmp/core_data.tar.gz /app/data    > /dev/null
+docker compose exec core     tar -czvf /tmp/core_data.tar.gz -C /app/data/ . > /dev/null
 docker cp "${TMP_CORE_NAME}:/tmp/core_data.tar.gz" "${backup_dir}/core_data.tar.gz"
 docker compose exec core     rm -f /tmp/core_data.tar.gz
-docker compose exec database pg_dump --clean -U taranis                  > "${backup_dir}/database_backup.sql"
-
+docker compose exec database pg_dumpall -U taranis                      > "${backup_dir}/database_backup.sql"
--- docker/restore.sh
+++ docker/restore.sh
@@ -8,38 +8,65 @@ usage() {
     exit 1
 }
 
+
+
 # Function to check if a volume is empty using a temporary container
-check_volume_empty() {   ## TODO rename check_volume_exists
+check_volume_exists() {
     local volume_name=$1
-    docker volume ls | grep -q $database_volume_name && exit "Database volume exists"  ## TODO: more verbose like below
-    docker volume ls | grep -q $core_volume_name     && exit "Core data volume ${core_volume_name} already exists, ensure you have a backup of your data and delete the volume before contiouing your restore"
+    if docker volume ls | grep  $volume_name; then 
+    exit "Volume ${volume_name} already exists, ensure you have a backup of your data and delete the volume before continuing your restore"
+    fi
 }
 
 # Function to restore PostgreSQL database from a backup file
 restore_postgresql() {
     local backup_file="$1"
     echo "Restoring PostgreSQL database from $backup_file..."
-	 volume_name=TODO_IMPLEMENT
-	 backup_dir=TODO_IMPLEMENT
-    # dropdb taranis -U taranis --force
-    # createuser --superuser -U taranis tmp
-    # createdb --owner taranis -U taranis taranis
-	 docker run --rm \
-		 -e TARAINS_DB="${DB_DATABASE:-taranis}" \
-		 -e TARANIS_USER="${DB_USER:-taranis}" \
-		 -e TARANIS_PASSWORD="${POSTGRES_PASSWORD:-taranis}" \
-		 -v $volume_name:/var/lib/postgresql/data \
-		 -v $backup_dir:/docker-entrypoint-initdb.d \
-		 --name taranis_database_restore docker.io/library/postgres:14
+    docker run --rm -d \
+        -e POSTGRES_DB="${DB_DATABASE:-taranis}" \
+        -e POSTGRES_USER="${DB_USER:-taranis}" \
+        -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-taranis}" \
+        -v ./$backup_dir:/docker-entrypoint-initdb.d \
+        -v $database_volume_name:/var/lib/postgresql/data \
+        --name taranis_database_restore docker.io/library/postgres:14
 }
 
 # Function to restore data to a Docker volume
 restore_volume_data() {
     local backup_file="$1"
-    local volume_name="$2"
-    echo "Restoring data to $volume_name from $backup_file..."
-    docker run --rm -v "$volume_name:/data" -v ./backups:/backups:Z busybox tar xzvf "$backup_file" -C /data
+
+    echo "Restoring data to $core_volume_name from $backup_file..."
+    docker run --rm -d --name taranis_core_restore \
+    -v "$core_volume_name:/app/data" -v ./backups:/backups:z busybox \
+    tar xzvf '$backup_file' -C /app/data
 }
+
+get_compose_volume_name() {
+    COMPOSE_FILE="compose.yml"
+    if [ ! -f "$COMPOSE_FILE" ]; then
+    echo "$COMPOSE_FILE not found!"
+    exit 1
+    fi
+
+    database_volume_name=""
+    core_volume_name=""
+
+    volume_lines=$(awk '/volumes:/ {flag=1; next} flag' $COMPOSE_FILE)
+
+    while read -r line; do
+    volume_name=$(echo "$line" | awk '{print $1}')
+    
+    # Prepend project name
+    prefixed_volume_name="${COMPOSE_PROJECT_NAME}_${volume_name%:}"
+
+    # Check for substrings and assign to correct variable
+    if [[ "$volume_name" == *database* ]]; then
+        database_volume_name="$prefixed_volume_name"
+    elif [[ "$volume_name" == *core* ]]; then
+        core_volume_name="$prefixed_volume_name"
+    fi
+    done <<< "$volume_lines"
+    }
 
 # Main script execution starts here
 
@@ -51,7 +78,9 @@ fi
 
 [[ -f .env ]] && source .env
 
+
 backup_dir=$1
+backup_dir="${backup_dir%/}"
 database_backup_file="$backup_dir/database_backup.sql"
 core_data_backup_file="$backup_dir/core_data.tar.gz"
 compose_project_name=${COMPOSE_PROJECT_NAME:-$(basename $(pwd))}
@@ -65,11 +94,16 @@ fi
 
 # Check if the necessary volumes are empty
 echo "Checking if the necessary volumes are empty..."
-check_volume_empty "core_data"
-check_volume_empty "database_data"
+check_volume_exists "core_data"
+check_volume_exists "database_data"
 
+get_compose_volume_name
+echo $core_volume_name
+echo $database_volume_name
 # Perform the actual restore operations
 restore_postgresql "$database_backup_file"
-restore_volume_data "$core_data_backup_file" "core_data"
+restore_volume_data "$core_data_backup_file"
+
+docker rm -f taranis_database_restore
 
 echo "Restore completed successfully."
