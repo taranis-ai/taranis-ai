f52bb1c195ad447d7381edae07f30d38e3c274a8
Peter Leitmann <peto.leitmann@gmail.com>
2024-08-16 19:22:39 +0200

    improve error handling of playwright manager

--- src/worker/worker/collectors/playwright_manager.py
+++ src/worker/worker/collectors/playwright_manager.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright, BrowserContext
+from playwright.sync_api import sync_playwright, BrowserContext, TimeoutError
 from urllib.parse import urlparse
 
 from worker.log import logger
@@ -10,7 +10,6 @@ class PlaywrightManager:
         self.browser = self.playwright.chromium.launch(proxy=self.parse_proxies(proxies))
         self.context = self.setup_context(headers)
         self.page = self.context.new_page()
-
 
     def setup_context(self, headers: dict | None = None) -> BrowserContext:
         if None not in headers.values():
@@ -26,26 +25,39 @@ class PlaywrightManager:
         password = parsed_url.password or ""
         logger.debug(f"Setting up proxy with user: {username}")
         return {"server": parsed_url.geturl(), "username": username, "password": password}
-    
+
     def stop_playwright_if_needed(self) -> None:
         if self.context:
             self.context.close()
+            logger.debug("Playwright context closed")
+        else:
+            logger.debug("No Playwright context to close")
+
         if self.browser:
             self.browser.close()
+            logger.debug("Playwright browser closed")
+        else:
+            logger.debug("No Playwright browser to close")
+
         if self.playwright:
             self.playwright.stop()
-        logger.debug("Playwright context stopped")
+            logger.debug("Playwright context stopped")
+        else:
+            logger.debug("No Playwright to stop")
 
     def fetch_content_with_js(self, url: str, xpath: str = "") -> str:
-        logger.debug(f"Getting web content with JS for {url} and XPATH {xpath}")
+        logger.debug(f"Getting web content with JS for {url} and XPATH {xpath=}")
+        try:
+            self.page.goto(url)
 
-        self.page.goto(url)
+            if xpath:
+                locator = self.page.locator(f"xpath={xpath}")
+                locator.wait_for(state="visible")
+                return self.page.content() or ""
 
-        if xpath:
-            locator = self.page.locator(f"xpath={xpath}")
-            locator.wait_for(state="visible")
-            return self.page.content() or ""
-
-        self.page.wait_for_load_state("networkidle")
-
+            self.page.wait_for_load_state("networkidle")
+        except TimeoutError as e:
+            logger.error(f"Fetching content with JS for {url} with {xpath=} has timed out, invalid XPath could be the reason, check for details. \nDetails: \n{str(e)}")
+        except Exception as e:
+            logger.error(f"Error fetching content with JS: {str(e)}")
         return self.page.content() or ""
