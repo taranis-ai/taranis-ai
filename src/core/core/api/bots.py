2a61f4933d93cac322e1a8cbade64a8daf38e9fb
Stefania Sava <sava.stefania@yahoo.com>
2024-10-16 20:14:20 +0200

    fix: UpdateNewsItemAttributes needs a return statement

--- src/core/core/api/bots.py
+++ src/core/core/api/bots.py
@@ -71,7 +71,8 @@ class NewsItem(MethodView):
 class UpdateNewsItemAttributes(MethodView):
     @api_key_required
     def put(self, news_item_id):
-        news_item.NewsItem.update_attributes(news_item_id, request.json)
+        return news_item.NewsItem.update_attributes(news_item_id, request.json)
+
 
 
 class StoryAttributes(MethodView):
