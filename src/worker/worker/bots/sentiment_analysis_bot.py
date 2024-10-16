5f46b9221b77de9794063f1faa091383fa96bc62
Peter Leitmann <peto.leitmann@gmail.com>
2024-10-16 19:40:39 +0200

    Fix iteration through a story

--- src/worker/worker/bots/sentiment_analysis_bot.py
+++ src/worker/worker/bots/sentiment_analysis_bot.py
@@ -2,19 +2,20 @@ from .base_bot import BaseBot
 from worker.log import logger
 from sentiment_analysis.sentiment_analysis_multimodel import analyze_sentiment, categorize_text
 
+
 class SentimentAnalysisBot(BaseBot):
     def __init__(self):
         super().__init__()
         self.type = "SENTIMENT_ANALYSIS_BOT"
         self.name = "Sentiment Analysis Bot"
         self.description = "Bot to analyze the sentiment of news items' content"
-    
+
     def execute(self, parameters: dict | None = None) -> dict:
         if not parameters:
             parameters = {}
         if not (data := self.get_stories(parameters)):
             return {"message": "No stories found for sentiment analysis"}
-        
+
         logger.info(f"Analyzing sentiment for {len(data)} news items")
 
         # Process each story
@@ -22,36 +23,33 @@ class SentimentAnalysisBot(BaseBot):
         self.update_news_items(sentiment_results)
 
         logger.info(f"Sentiment analysis complete with results: {sentiment_results}")
-        self.update_news_items(sentiment_results)
-
 
         return {
-            "sentiment_score": sentiment_results.get(list(sentiment_results.keys())[0])["sentiment"],  
-            "message": "Sentiment analysis complete"
-            }
-    
+            "sentiment_score": sentiment_results.get(list(sentiment_results.keys())[0])["sentiment"],
+            "message": "Sentiment analysis complete",
+        }
+
     def analyze_stories(self, stories: list) -> dict:
         results = {}
-        for story in stories:
-            text_content = self.extract_content_from_story(story)
+        news_items = stories[0].get("news_items", [])
+        for news_item in news_items:
+            text_content = news_item.get("content", "")
             logger.info(f"Extracted text for sentiment analysis: {text_content}")
 
             sentiment = analyze_sentiment(text_content)
             if "score" not in sentiment:
-                logger.error(f"Sentiment analysis failed for story {story['id']}:")
+                logger.error(f"Sentiment analysis failed for story {news_item['id']}:")
                 continue
 
-
             category = categorize_text(sentiment)
-
-            results[story["id"]] = {
+            results[news_item["id"]] = {
                 "sentiment": sentiment["score"],
                 "category": category,
             }
 
         return results
 
-    def extract_content_from_story(self, story: dict) -> str:#
+    def extract_content_from_story(self, story: dict) -> str:  #
         content = [item.get("content", "") for item in story.get("news_items", [])]
         return " ".join(content)
 
