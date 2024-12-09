e912bef766e6f95bb8bf310b60ff6664111b7f23
Peter Leitmann <peto.leitmann@gmail.com>
2024-12-09 23:40:07 +0100

    Create a story connector sharing dialog

---
+++ src/gui/src/components/popups/PopupShareToConnector.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card>
+    <v-card-title> Add to Report </v-card-title>
+    <v-card-text>
+      Select a connector:
+      <v-autocomplete
+        v-model="connectorSelection"
+        :items="connectors"
+        item-title="name"
+        item-value="id"
+        single-line
+        label="Select Connector"
+        no-data-text="No connectors found"
+        menu-icon="mdi-chevron-down"
+      />
+      <v-spacer />
+    </v-card-text>
+    <v-card-actions class="mt-1">
+      <v-btn
+        variant="outlined"
+        class="text-lowercase text-red-darken-3 ml-3"
+        prepend-icon="mdi-close"
+        @click="close"
+      >
+        Abort
+      </v-btn>
+      <v-spacer />
+      <v-btn
+        variant="outlined"
+        class="text-lowercase text-primary mr-3"
+        prepend-icon="mdi-share-outline"
+        @click="share"
+      >
+        Share with Connector
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+import { useConfigStore } from '@/stores/ConfigStore'
+import { ref, onMounted } from 'vue'
+export default {
+  name: 'PopupShareToConnector',
+
+  emits: ['close'],
+  setup(props, { emit }) {
+    const configStore = useConfigStore()
+    const connectors = ref([])
+    const connectorSelection = ref(null)
+    const share = () => {
+      console.log('Selected connector ID:', connectorSelection.value)
+      emit('close')
+    }
+    const close = () => {
+      emit('close')
+    }
+    onMounted(() => {
+      configStore.loadConnectors().then(() => {
+        connectors.value = configStore.connectors.items
+      })
+    })
+    return {
+      connectorSelection,
+      connectors,
+      share,
+      close
+    }
+  }
+}
+</script>
