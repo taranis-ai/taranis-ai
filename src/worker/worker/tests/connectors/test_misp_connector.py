3b20cb3aabfc7063b39a452b37c8879f8fe4accf
Peter Leitmann <peto.leitmann@gmail.com>
2025-01-07 16:04:52 +0100

    Add basic template unit test

---
+++ src/worker/worker/tests/connectors/test_misp_connector.py
@@ -0,0 +1,16 @@
+import worker.connectors as connectors
+
+
+def test_object_keys_completeness(misp_template):
+    """Test that the object data keys match the template keys"""
+    object_data = connectors.MISPConnector.get_news_item_object_dict()
+
+    template_keys = set(misp_template["attributes"].keys())
+    object_data_keys = set(object_data.keys())
+
+    missing_keys = template_keys - object_data_keys
+    extra_keys = object_data_keys - template_keys
+
+    assert len(missing_keys) == 0, f"Missing keys in object_data: {missing_keys}"
+    assert len(extra_keys) == 0, f"Extra keys in object_data: {extra_keys}"
+    assert template_keys == object_data_keys, "Object data keys do not match the template"
