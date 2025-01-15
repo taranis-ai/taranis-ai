c8f2899fe39a78993815eb834c06f02f67f6c0e3
Armin Schreger <armin@schreger.at>
2025-01-15 17:35:28 +0100

    Add scripts for local development

---
+++ src/frontend/extend_tmux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+# cd $(git rev-parse --show-toplevel)
+
+# Create tailwind tab
+# tmux new-window -t taranis -n tailwind
+# tmux send-keys -t taranis:tailwind "./install_and_run_tailwind.sh" C-m
+
+# Create frontend tab
+tmux new-window -t taranis -n frontend
+tmux send-keys -t taranis:frontend "./install_and_run_dev.sh" C-m
