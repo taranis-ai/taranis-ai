cdf5027ce8e86b0647c7e8e526f8787fbba5a485
Peter Leitmann <peto.leitmann@gmail.com>
2025-02-04 14:11:34 +0100

    Remove APscheduler direct interval mapping - keep cron-only

--- src/gui/src/components/config/TimeIntervalFields.vue
+++ src/gui/src/components/config/TimeIntervalFields.vue
@@ -1,107 +1,37 @@
 <template>
   <v-container>
-    <v-row class="mt-3">
-      <v-col cols="2">
-        <v-text-field
-          v-model="intervalData.weeks"
-          type="number"
-          label="Weeks"
-          variant="outlined"
-        />
-      </v-col>
-      <v-col cols="2">
-        <v-text-field
-          v-model="intervalData.days"
-          type="number"
-          label="Days"
-          variant="outlined"
-        />
-      </v-col>
-      <v-col cols="2">
-        <v-text-field
-          v-model="intervalData.hours"
-          type="number"
-          label="Hours"
-          variant="outlined"
-        />
-      </v-col>
-      <v-col cols="2">
-        <v-text-field
-          v-model="intervalData.minutes"
-          type="number"
-          label="Minutes"
-          variant="outlined"
-        />
-      </v-col>
-      <v-col cols="2">
-        <v-text-field
-          v-model="intervalData.seconds"
-          type="number"
-          label="Seconds"
-          variant="outlined"
-        />
-      </v-col>
-    </v-row>
+    <v-card class="pa-4 mb-4">
+      <v-card-title class="text-h6">Refresh Interval</v-card-title>
+      <v-divider></v-divider>
 
-    <v-row class="mt-3">
-      <v-col cols="5">
-        <date-filter
-          placeholder="Start Date"
-          tooltip-text="Date when the refresh interval starts to be applied"
-        />
-      </v-col>
-      <v-col cols="5">
-        <date-filter
-          placeholder="End Date"
-          tooltip-text="Date when the refresh interval starts to be applied"
-        />
-      </v-col>
-      <v-col cols="2">
-        <v-text-field
-          v-model="intervalData.jitter"
-          type="number"
-          label="Jitter (s)"
-          variant="outlined"
-        />
-      </v-col>
-    </v-row>
+      <v-card-text>
+        <v-row class="mt-3">
+          <v-col cols="12">
+            <cron-vuetify v-model="value" @error="error = $event" />
+          </v-col>
+        </v-row>
 
-    <v-row class="mt-3">
-      <v-col cols="12">
-        <cron-vuetify v-model="value" @error="error = $event" />
-      </v-col>
-    </v-row>
-
-    <v-row class="mt-3">
-      <v-col cols="12">
-        <v-text-field
-          class="pt-3"
-          :modelValue="value"
-          @update:model-value="nextValue = $event"
-          @blur="value = nextValue"
-          label="cron expression"
-          :error-messages="error"
-          variant="outlined"
-        />
-      </v-col>
-    </v-row>
+        <v-row class="mt-3">
+          <v-col cols="3">
+            <v-text-field
+              class="pt-3 cron-input"
+              :modelValue="value"
+              @update:model-value="nextValue = $event"
+              @blur="value = nextValue"
+              label="Cron Expression"
+              :error-messages="error"
+              variant="outlined"
+            />
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script setup>
-import dateFilter from '@/components/common/filter/dateFilter.vue'
+import { ref } from 'vue'
 import { CronVuetify } from '@vue-js-cron/vuetify'
-import { defineProps, defineEmits, computed, ref } from 'vue'
-
-const props = defineProps({
-  modelValue: Object
-})
-const emit = defineEmits(['update:modelValue'])
-
-const intervalData = computed({
-  get: () => props.modelValue || {},
-  set: (value) => emit('update:modelValue', value)
-})
 
 const value = ref('* * * * *')
 const nextValue = ref('* * * * *')
@@ -110,4 +40,8 @@ const error = ref('')
 
 <style scoped>
 @import '@vue-js-cron/vuetify/dist/vuetify.css';
+
+.cron-input {
+  width: 100%;
+}
 </style>
