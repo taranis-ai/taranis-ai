0739415621c0dd0b1e113212af21d8aa2339837d
llinauer <l.linauer@gmx.at>
2025-02-05 17:14:28 +0100

    Change type hints in http_backend.py to python3.10+ style

--- src/worker/worker/http_backend.py
+++ src/worker/worker/http_backend.py
@@ -1,15 +1,22 @@
 from celery.backends.base import BaseBackend
 from worker.core_api import CoreApi
-from typing import Optional
 from celery.app.task import Context
+
 
 class HTTPBackend(BaseBackend):
     def __init__(self, app=None, **kwargs):
         super().__init__(app, **kwargs)
         self.core_api = CoreApi()
 
-    def store_result(self, task_id: str, result: Optional[dict | str | Exception], state: str,
-                     traceback: Optional[str]=None, request: Optional[Context]=None, **kwargs):
+    def store_result(
+        self,
+        task_id: str,
+        result: dict | str | Exception | None,
+        state: str,
+        traceback: str | None = None,
+        request: Context | None = None,
+        **kwargs,
+    ):
         data = {
             "task_id": task_id,
             "status": state,
