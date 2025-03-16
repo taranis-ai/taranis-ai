06d797b20ad150fa2b8b262b8613c3226260f83f
Benjamin Akhras <b@akhras.at>
2025-03-16 13:04:11 +0100

    blubb^2

--- src/worker/worker/tasks.py
+++ src/worker/worker/tasks.py
@@ -6,16 +6,16 @@ def setup_tasks():
     import worker.misc.misc_tasks  # noqa: F401
 
     if "Bots" in Config.WORKER_TYPES:
-        from worker.bots.bot_tasks import BotTask  # noqa: F401
+        import worker.bots.bot_tasks  # noqa: F401
 
     if "Collectors" in Config.WORKER_TYPES:
-        from worker.collectors.collector_tasks import CollectorTask  # noqa: F401
+        import worker.collectors.collector_tasks  # noqa: F401
 
     if "Presenters" in Config.WORKER_TYPES:
         try:
-            from worker.presenters.presenter_tasks import PresenterTask  # noqa: F401
+            import worker.presenters.presenter_tasks  # noqa: F401
         except OSError as e:
             logger.critical(f"Failed to load PDFPresenter: {e}. Ensure WeasyPrint and dependencies are installed.")
 
     if "Publishers" in Config.WORKER_TYPES:
-        from worker.publishers.publisher_tasks import PublisherTask  # noqa: F401
+        import worker.publishers.publisher_tasks  # noqa: F401
