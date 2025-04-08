ba2b488b5ec8f6fdc41734a33354fd6a206f13de
Benjamin Akhras <b@akhras.at>
2025-04-08 10:29:44 +0200

    add assess models

--- src/models/models/base.py
+++ src/models/models/base.py
@@ -1,14 +1,12 @@
 from pydantic import BaseModel
 from typing import ClassVar, TypeVar
 
-from frontend.config import Config
-
 T = TypeVar("T", bound="TaranisBaseModel")
 
 
 class TaranisBaseModel(BaseModel):
     _core_endpoint: ClassVar[str]
-    _cache_timeout: ClassVar[int] = Config.CACHE_DEFAULT_TIMEOUT
+    _cache_timeout: ClassVar[int]
 
     def model_dump(self, *args, **kwargs):
         kwargs.setdefault("exclude_none", True)
