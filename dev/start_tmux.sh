2c3ca86eb90983008c7e11fed401b8096536a13a
Benjamin Akhras <b@akhras.at>
2025-04-08 10:58:19 +0200

    update dev setup for new frontend

--- dev/start_tmux.sh
+++ dev/start_tmux.sh
@@ -16,6 +16,12 @@ tmux send-keys -t taranis:gui "./install_and_run_dev.sh" C-m
 tmux new-window -t taranis:2 -n worker -c src/worker
 tmux send-keys -t taranis:worker "./install_and_run_dev.sh" C-m
 
+# Create Frontend tabs
+tmux new-window -t taranis:3 -n tailwind -c src/frontend
+tmux send-keys -t taranis:tailwind "./install_and_run_tailwind.sh" C-m
+tmux new-window -t taranis:4 -n frontend -c src/frontend
+tmux send-keys -t taranis:frontend "./install_and_run_dev.sh" C-m
+
 # Attach to the session
 tmux attach-session -t taranis
 
