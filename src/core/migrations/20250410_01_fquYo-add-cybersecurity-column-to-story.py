8932ae560d2dedbaf05723a731644191812f06e5
llinauer <l.linauer@gmx.at>
2025-04-16 11:22:50 +0200

    Change dependencies of migrations after merge of master

--- src/core/migrations/20250410_01_fquYo-add-cybersecurity-column-to-story.py
+++ src/core/migrations/20250410_01_fquYo-add-cybersecurity-column-to-story.py
@@ -4,6 +4,6 @@ add cybersecurity column to story
 
 from yoyo import step
 
-__depends__ = {"20250228_01_Pnb5c-refactor-organization"}
+__depends__ = {"20250324_02_vL3hN-add-last-change-column"}
 
 steps = [step("ALTER TABLE story ADD column cybersecurity VARCHAR(10);")]
