35f54f8173052c412229cc4d96e86176c3fa04d7
b3n <benjamin.akhras@ait.ac.at>
2025-05-22 09:57:34 +0200

    Refactor code structure for improved readability and maintainability (#524)
    Fix bug while building assets for production

--- src/frontend/install_and_run_tailwind.sh
+++ src/frontend/install_and_run_tailwind.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-set -eu
-
-if [ ! -x tailwindcss ]; then
-    curl -sLo tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-    chmod +x tailwindcss
-fi
-
-./update_assets.sh
-
-./tailwindcss -i frontend/static/css/input.css -o frontend/static/css/tailwind.css --watch
-
+./build_tailwindcss.sh watch
