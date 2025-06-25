f7f38857775ac0efa9150a81313e9b27a4da9e16
Armin Schreger <armin@schreger.at>
2025-06-25 16:33:07 +0200

    Fix frontend tests to load .env

--- src/frontend/tests/conftest.py
+++ src/frontend/tests/conftest.py
@@ -1,10 +1,16 @@
 import os
+import sys
 import pytest
 from dotenv import load_dotenv
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 env_file = os.path.join(base_dir, ".env")
-load_dotenv(dotenv_path=env_file, override=False)
+current_path = os.getcwd()
+
+if not current_path.endswith("src/frontend"):
+    sys.exit("Tests must be run from within src/frontend")
+
+load_dotenv(dotenv_path=env_file, override=True)
 
 
 @pytest.fixture(scope="session")
