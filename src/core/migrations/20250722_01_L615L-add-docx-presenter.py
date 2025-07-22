189cae18ff147e3257b4e08137ce1663037a8e67
llinauer <l.linauer@gmx.at>
2025-07-22 11:48:02 +0200

    Add migration for docx presenter

---
+++ src/core/migrations/20250722_01_L615L-add-docx-presenter.py
@@ -0,0 +1,12 @@
+"""
+add_docx_presenter
+"""
+
+from yoyo import step
+
+__depends__ = {"20250530_01_bgjgJ-add-ppn-collector"}
+
+steps = [
+    step("ALTER TYPE worker_types ADD VALUE IF NOT EXISTS 'DOCX_PRESENTER';"),
+    step("ALTER TYPE presenter_types ADD VALUE IF NOT EXISTS 'DOCX_PRESENTER';"),
+]
