ddc10a8a866cf835f9bdf312e3944be48de2fbd3
llinauer <l.linauer@gmx.at>
2025-07-29 12:41:13 +0200

    Register the product_type_parameters frontend endpoint

--- src/frontend/frontend/router/admin.py
+++ src/frontend/frontend/router/admin.py
@@ -123,6 +123,13 @@ class PublisherParameterAPI(MethodView):
         return PublisherView.get_publisher_parameters_view(publisher_id, publisher_type)
 
 
+class ProductTypeParameterAPI(MethodView):
+    @auth_required()
+    def get(self):
+        presenter_type = request.args.get("type", "")
+        return ProductTypeView.get_product_type_parameters_view(presenter_type)
+
+
 class ReportItemTypeGroupsAPI(MethodView):
     @auth_required()
     def post(self):
@@ -205,6 +212,7 @@ def init(app: Flask):
 
     admin_bp.add_url_rule("/product_types", view_func=ProductTypeView.as_view("product_types"))
     admin_bp.add_url_rule("/product_types/<int:product_type_id>", view_func=ProductTypeView.as_view("edit_product_type"))
+    admin_bp.add_url_rule("/product_type_parameters", view_func=ProductTypeParameterAPI.as_view("product_type_parameters"))
 
     admin_bp.add_url_rule("/templates", view_func=TemplateView.as_view("template_data"))
     admin_bp.add_url_rule("/templates/<string:template>", view_func=TemplateView.as_view("edit_template"))
