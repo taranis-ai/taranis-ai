e418255f7928b5680bf13cee4df871ea31c63e9d
llinauer <l.linauer@gmx.at>
2025-08-20 08:57:49 +0200

    Move extra config from ProductParameterValue to TaranisBaseModel

--- src/models/models/admin.py
+++ src/models/models/admin.py
@@ -1,4 +1,4 @@
-from pydantic import Field, AnyUrl, Extra
+from pydantic import Field, AnyUrl
 from typing import Literal, Any
 from datetime import datetime
 
@@ -195,9 +195,6 @@ class OSINTSourceGroup(TaranisBaseModel):
 
 class ProductParameterValue(TaranisBaseModel):
     TEMPLATE_PATH: str | None = None
-
-    class Config:
-        extra = Extra.allow
 
 
 class ProductType(TaranisBaseModel):
