b15d2b5471261c9c452b99596900b2d039b242d7
Armin Schreger <armin@schreger.at>
2025-08-20 15:41:30 +0200

    Reenable hashlib.md5 option

--- src/core/core/managers/data_manager.py
+++ src/core/core/managers/data_manager.py
@@ -8,7 +8,7 @@ import hashlib
 import base64
 
 def file_hash(file_path):
-    hash_md5 = hashlib.md5()
+    hash_md5 = hashlib.md5(usedforsecurity=False)
     with open(file_path, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
