b41a562e1ec1187d81a4f02251aecc4203906692
Benjamin Akhras <b@akhras.at>
2025-08-27 15:17:13 +0200

    fix connectors admin

--- src/frontend/frontend/views/connector_views.py
+++ src/frontend/frontend/views/connector_views.py
@@ -1,5 +1,6 @@
 from frontend.views.base_view import BaseView
 from models.admin import Connector
+from models.types import CONNECTOR_TYPES
 from frontend.filters import render_icon, render_source_parameter, render_worker_status, render_truncated
 
 from typing import Any
@@ -11,6 +12,26 @@ class ConnectorView(BaseView):
     _index = 115
 
     _read_only = True  # TODO: Remove this when we implement create/update/delete for Connectors
+
+    connector_types = {
+        member.name.lower(): {"id": member.name.lower(), "name": " ".join(part.capitalize() for part in member.name.split("_"))}
+        for member in CONNECTOR_TYPES
+    }
+
+    @classmethod
+    def get_extra_context(cls, base_context: dict) -> dict[str, Any]:
+        parameters = {}
+        parameter_values = {}
+
+        connector = base_context.get(cls.model_name())
+        if connector and (connector_type := connector.type):
+            parameter_values = connector.parameters
+            parameters = cls.get_worker_parameters(worker_type=connector_type.name.lower())
+
+        base_context["parameters"] = parameters
+        base_context["parameter_values"] = parameter_values
+        base_context["connector_types"] = cls.connector_types.values()
+        return base_context
 
     @classmethod
     def get_columns(cls) -> list[dict[str, Any]]:
--- src/frontend/install_and_run_dev.sh
+++ src/frontend/install_and_run_dev.sh
@@ -5,6 +5,5 @@ set -eu
 uv sync --all-extras --frozen --python 3.13
 
 source .venv/bin/activate
-uv pip install -e ../models
 
 python -m flask run
