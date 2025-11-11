03cfa2e69cd005271d3df533967e7a165aa02820
Armin Schreger <armin@schreger.at>
2025-11-11 16:35:33 +0100

    Refactor install_and_run_dev.sh scripts  (#695)
    Start python process with uv

--- src/worker/install_and_run_dev.sh
+++ src/worker/install_and_run_dev.sh
@@ -5,10 +5,4 @@ set -eu
 uv sync --all-extras --frozen --python 3.13 --no-install-package taranis-models
 uv pip install -e ../models
 
-# Set PYTHONPATH for local development to include models directory
-export PYTHONPATH="../models:${PYTHONPATH:-}"
-
-source .venv/bin/activate
-
-python -m worker.start
-
+uv run python ./start_dev_worker.py
