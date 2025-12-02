6076747edca3f3cab3337c8cfe10044f22a39297
Armin Schreger <armin@schreger.at>
2025-12-02 17:43:29 +0100

    feat(core): improve error handling for OSINT source icon upload and API endpoints

--- src/core/core/api/config.py
+++ src/core/core/api/config.py
@@ -548,8 +548,11 @@ class OSINTSources(MethodView):
 
     @auth_required("CONFIG_OSINT_SOURCE_CREATE")
     def post(self):
-        if source := osint_source.OSINTSource.add(request.json):
-            return {"id": source.id, "message": "OSINT source created successfully"}, 201
+        try:
+            if source := osint_source.OSINTSource.add(request.json):
+                return {"id": source.id, "message": "OSINT source created successfully"}, 201
+        except ValueError as exc:
+            return {"error": str(exc)}, 400
         return {"error": "OSINT source could not be created"}, 400
 
     @auth_required("CONFIG_OSINT_SOURCE_UPDATE")
@@ -560,7 +563,7 @@ class OSINTSources(MethodView):
             if source := osint_source.OSINTSource.update(source_id, update_data):
                 return {"message": f"OSINT Source {source.name} updated", "id": f"{source_id}"}, 200
         except ValueError as e:
-            return {"error": str(e)}, 500
+            return {"error": str(e)}, 400
         return {"error": f"OSINT Source with ID: {source_id} not found"}, 404
 
     @auth_required("CONFIG_OSINT_SOURCE_DELETE")
