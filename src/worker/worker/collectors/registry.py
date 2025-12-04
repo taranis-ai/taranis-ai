416055d58db9f2401463224db62524724e2aaec8
Stefania Sava <sava.stefania@yahoo.com>
2025-12-04 12:09:32 +0100

    fix a misp inport

--- src/worker/worker/collectors/registry.py
+++ src/worker/worker/collectors/registry.py
@@ -1,11 +1,11 @@
 from worker.collectors.rss_collector import RSSCollector
 from worker.collectors.rt_collector import RTCollector
 from worker.collectors.simple_web_collector import SimpleWebCollector
-from worker.collectors.misp_collector import MISPCollector
+from worker.collectors.misp_collector import MispCollector
 
 COLLECTOR_REGISTRY = {
     "rss_collector": RSSCollector,
     "rt_collector": RTCollector,
     "simple_web_collector": SimpleWebCollector,
-    "misp_collector": MISPCollector,
+    "misp_collector": MispCollector,
 }
