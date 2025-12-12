2f69a7059fe42fe6999f61ac430019d7c325de52
Peter Leitmann <peter.leitmann@ait.ac.at>
2025-12-12 13:57:31 +0100

    Cleanup vendor.js

--- src/frontend/vendor.js
+++ src/frontend/vendor.js
@@ -8,22 +8,12 @@ import Swal from "npm:sweetalert2";
 import { highlightSearchTerm } from "npm:highlight-search-term";
 
 import "npm:choices.js/public/assets/styles/choices.min.css";
+import "npm:htmx-ext-response-targets";
 import "npm:sweetalert2/dist/sweetalert2.min.css";
 
 window.htmx = htmx;
 globalThis.htmx = htmx;
 window.Alpine = Alpine;
-htmx.defineExtension("json-enc", {
-  onEvent(name, evt) {
-    if (name === "htmx:configRequest") {
-      evt.detail.headers["Content-Type"] = "application/json";
-    }
-  },
-  encodeParameters(xhr, parameters) {
-    xhr.overrideMimeType("text/json");
-    return JSON.stringify(parameters);
-  },
-});
 
 window.Sortable = Sortable;
 window.Choices = Choices;
@@ -31,14 +21,6 @@ window.Fuse = Fuse;
 window.Swal = Swal;
 window.highlightSearchTerm = highlightSearchTerm;
 
-(async () => {
-  if (!globalThis.htmx) {
-    globalThis.htmx = htmx;
-  }
-  await Promise.all([
-    import("npm:htmx-ext-response-targets"),
-  ]);
-})();
 
 if (typeof Alpine.start === "function") {
   if (document.readyState === "loading") {
