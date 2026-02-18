4cc5e459dd622ddaa27d668ac3007b12df96223d
Benjamin Akhras <b@akhras.at>
2026-02-18 14:26:54 +0100

    bump openpi yaml and swagger ui

--- src/core/core/api/static.py
+++ src/core/core/api/static.py
@@ -1,5 +1,6 @@
-from flask import Flask, send_from_directory, Blueprint
+from flask import Blueprint, Flask, send_from_directory
 from flask.views import MethodView
+
 from core.config import Config
 from core.managers.data_manager import get_default_json
 
@@ -14,9 +15,9 @@ class DefaultWordLists(MethodView):
         return send_from_directory(get_default_json("default_word_lists.json"), "default_word_lists.json")
 
 
-class OpenAPI3_1(MethodView):
+class OpenAPI3(MethodView):
     def get(self):
-        return send_from_directory("static", "openapi3_1.yaml")
+        return send_from_directory("static", "openapi3.yaml")
 
 
 def initialize(app: Flask):
@@ -24,6 +25,6 @@ def initialize(app: Flask):
 
     static_bp.add_url_rule("/default_sources.json", view_func=DefaultSources.as_view("default_source"))
     static_bp.add_url_rule("/default_word_lists.json", view_func=DefaultWordLists.as_view("default_word_lists"))
-    static_bp.add_url_rule("/openapi3_1.yaml", view_func=OpenAPI3_1.as_view("openapi3_1"))
+    static_bp.add_url_rule("/openapi3.yaml", view_func=OpenAPI3.as_view("openapi3"))
 
     app.register_blueprint(static_bp)
