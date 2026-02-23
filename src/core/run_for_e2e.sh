d8ef8f4eb45d003a841f235465e30203a313c770
Benjamin Akhras <b@akhras.at>
2026-02-23 16:30:05 +0100

    cleanup

--- src/core/run_for_e2e.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-uv sync --frozen --python 3.13 --no-install-package taranis-models
-uv pip install -e ../models
-
-uv run --no-sync --frozen python -m flask run --no-reload --port ${1:-5000}
-
