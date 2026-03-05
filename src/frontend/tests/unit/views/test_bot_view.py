b3c6342a3ef3cd27ee0172c62b6bc692f4c99eba
llinauer <l.linauer@gmx.at>
2026-03-05 13:19:11 +0100

    Test ordering in test_bot_view.py

--- src/frontend/tests/unit/views/test_bot_view.py
+++ src/frontend/tests/unit/views/test_bot_view.py
@@ -2,6 +2,8 @@ import pytest
 from flask import url_for
 from lxml import html
 
+from frontend.views.admin_views.bot_views import BotView
+
 
 @pytest.mark.parametrize(
     "bot_type",
@@ -29,3 +31,37 @@ def test_bot_parameters_include_optional_positive_integer_requests_timeout(authe
     assert requests_timeout_fields[0].get("pattern") == "^[1-9][0-9]*$"
     assert requests_timeout_fields[0].get("required") is None
     assert response.text.index('name="parameters[ITEM_FILTER]"') < response.text.index('name="parameters[REQUESTS_TIMEOUT]"')
+
+
+def test_reorder_bot_parameters_uses_bot_specific_order_and_keeps_unknowns_stable():
+    parameters = [
+        {"name": "REFRESH_INTERVAL"},
+        {"name": "UNKNOWN_ONE"},
+        {"name": "BOT_API_KEY"},
+        {"name": "ITEM_FILTER"},
+        {"name": "UNKNOWN_TWO"},
+        {"name": "REQUESTS_TIMEOUT"},
+    ]
+
+    ordered = BotView._reorder_bot_parameters("story_bot", parameters)
+
+    assert [param["name"] for param in ordered] == [
+        "ITEM_FILTER",
+        "REQUESTS_TIMEOUT",
+        "BOT_API_KEY",
+        "REFRESH_INTERVAL",
+        "UNKNOWN_ONE",
+        "UNKNOWN_TWO",
+    ]
+
+
+def test_reorder_bot_parameters_returns_original_order_for_unknown_bot_type():
+    parameters = [{"name": "B"}, {"name": "A"}, {"name": "C"}]
+
+    ordered = BotView._reorder_bot_parameters("unknown_bot_type", parameters)
+
+    assert [param["name"] for param in ordered] == [
+        "B",
+        "A",
+        "C",
+    ]
