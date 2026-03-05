072d85a724424893b8b370927b76bfe435e410ff
llinauer <l.linauer@gmx.at>
2026-03-05 13:04:35 +0100

    Parametrize BotApi timeout resolve tests; give more meaningful name

--- src/worker/worker/tests/bots/test_bot_api.py
+++ src/worker/worker/tests/bots/test_bot_api.py
@@ -1,17 +1,19 @@
+import pytest
+
 from worker.bot_api import BotApi
 from worker.config import Config
 
 
-def test_bot_api_timeout_uses_parameter_override():
-    bot_api = BotApi("http://example.invalid", requests_timeout="42")
-    assert bot_api.timeout == 42
-
-
-def test_bot_api_timeout_falls_back_when_parameter_missing():
-    bot_api = BotApi("http://example.invalid", requests_timeout="")
-    assert bot_api.timeout == Config.REQUESTS_TIMEOUT
-
-
-def test_bot_api_timeout_falls_back_when_parameter_invalid():
-    bot_api = BotApi("http://example.invalid", requests_timeout="not-a-number")
-    assert bot_api.timeout == Config.REQUESTS_TIMEOUT
+@pytest.mark.parametrize(
+    "timeout_input, expected_value",
+    [
+        ("42", 42),
+        ("", Config.REQUESTS_TIMEOUT),
+        ("character", Config.REQUESTS_TIMEOUT),
+        (-1, Config.REQUESTS_TIMEOUT),
+        (0, Config.REQUESTS_TIMEOUT),
+    ],
+)
+def test_bot_api_timeout_resolve(timeout_input, expected_value):
+    bot_api = BotApi("http://example.invalid", requests_timeout=timeout_input)
+    assert bot_api.timeout == expected_value
