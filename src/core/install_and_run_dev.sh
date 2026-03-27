d79e4f626da6feb6ffad1271fa810094602f5ff6
Armin Schreger <armin@schreger.at>
2026-03-27 17:40:22 +0100

    Enable DEBUG and use venv python in dev scripts

--- src/core/install_and_run_dev.sh
+++ src/core/install_and_run_dev.sh
@@ -2,7 +2,17 @@
 
 set -eu
 
+case "${DEBUG:-}" in
+    ""|true|false|True|False|TRUE|FALSE|1|0|yes|no|on|off|YES|NO|ON|OFF)
+        ;;
+    *)
+        export DEBUG=true
+        ;;
+esac
+
+unset VIRTUAL_ENV
+
 uv sync --all-extras --frozen --python 3.13 --no-install-package taranis-models
-uv pip install -e ../models
+uv pip install --python .venv/bin/python -e ../models
 
 uv run --no-sync --frozen python -m flask run
