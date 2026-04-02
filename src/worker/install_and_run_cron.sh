bb4b159aa2187e626a4de367a124ef6fc0847c05
Armin Schreger <armin@schreger.at>
2026-04-02 14:48:04 +0200

    dev: simplify worker and app startup scripts

--- src/worker/install_and_run_cron.sh
+++ src/worker/install_and_run_cron.sh
@@ -2,14 +2,6 @@
 
 set -eu
 
-case "${DEBUG:-}" in
-    ""|true|false|True|False|TRUE|FALSE|1|0|yes|no|on|off|YES|NO|ON|OFF)
-        ;;
-    *)
-        export DEBUG=true
-        ;;
-esac
-
 unset VIRTUAL_ENV
 
 uv sync --all-extras --frozen --python 3.13 --no-install-package taranis-models
